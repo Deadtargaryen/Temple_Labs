60ae0883b031e79afea4e0eaeb36b9c19f5d8081
Deadtargaryen <mannyetukudoh@gmail.com>
2024-03-06 22:11:19 +0100

    completed Services.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Button from "./components/Button";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -12,7 +14,9 @@ const App = () => {
         <Header/>
         <Hero/>
         <Benefits/>
-        <Collaboration/>
+        <Collaboration/> 
+        <Services/>
+        <Pricing/>
       </div>
       <ButtonGradient />
     </>
